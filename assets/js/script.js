b63e8160c443fe3c52a93f06f84fd2055ec73e8f
unknown <bryansantos75@gmail.com>
2021-07-21 20:31:23 -0300

    js

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,6 +9,9 @@ por Seletor: querySelector()
 let nome = document.querySelector('#nome')
 let email = document.querySelector('#email')
 let assunto = document.querySelector('#assunto')
+let nomeOk = false
+let emailOk = false
+let assuntoOk = false
 
 nome.style.width = '98%'
 email.style.width = '98%'
@@ -21,6 +24,7 @@ function validaNome() {
     } else {
         txtNome.innerHTML = 'Nome válido'
         txtNome.style.color = 'green'
+        nomeOk = true
     }
 }
 function validaEmail() {
@@ -31,5 +35,24 @@ function validaEmail() {
     } else {
         txtEmail.innerHTML = 'E-mail válido'
         txtEmail.style.color = 'green'
+        emailOk = true
+    }
+}
+function validaAssunto() {
+    let txtAssunto = document.querySelector('#txtAssunto') 
+    if (assunto.value.length >= 100) {
+        txtAssunto.innerHTML = 'Digite no máximo 100 caracteres'
+        txtAssunto.style.color = 'red'
+        txtAssunto.style.display = 'block'
+    } else {
+        txtAssunto.style.display = 'none'
+        assuntoOk = true
+    }
+}
+function enviar() {
+    if (nomeOk == true && emailOk == true && assuntoOk == true) {
+        alert('Enviado com sucesso!!')
+    } else {
+        alert('Preencha corretamente antes de enviar...')
     }
 }
